0f8514b559fe949ed1175f43c3bed3cf9af4d706
Ayush <jaiswalaman231211@gmail.com>
2024-11-16 02:11:36 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,8 +11,14 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//router import
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
+
 export { app }
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from "./app.js"
 dotenv.config({
     path: './env'
 })
@@ -22,23 +22,22 @@ connectDB()
 
 
 
-/* const app = express()
- ( async () => {
-    try{
-        await mongoose.connect(`${process.env.
-            MONGODB_URI}/${DB_NAME}`)
-            app.on("error", (error) => {
-                console.log("ERRR:", error);
-                throw error
-            })
+//  const app = express()
+//  ( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.
+//             MONGODB_URI}/${DB_NAME}`)
+//             app.on("error", (error) => {
+//                 console.log("ERRR:", error);
+//                 throw error
+//             })
 
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on port ${process.env.PORT}`);
-            })
-    }catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`App is listening on port ${process.env.PORT}`);
+//             })
+//     }catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
 
-*/
